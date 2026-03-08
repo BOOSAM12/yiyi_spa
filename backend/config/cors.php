3437b4b5054f985017876296dfd9a32b400047b4
Sergio Arias <ariasmejiasergio13@gmail.com>
2026-03-08 15:16:20 -0500

    Verificando requerimientos tecnicos

--- backend/config/cors.php
+++
@@ -1,3 +0,0 @@
-'paths' => ['api/*', 'sanctum/csrf-cookie'],
-'allowed_origins' => ['http://localhost:5173'],
-'supports_credentials' => true,
--- frontend/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import AppRoutes from './routes/AppRoutes';
-import './index.css';
-
-export default function App() {
-  return <AppRoutes />
-}
-
